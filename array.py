0257e2550090a61058eb4a899c43f7bfe31a3df2
Tess Heiner <tessheiner@mail.weber.edu>
2022-04-06 18:33:57 -0600

    finished assignment

---
+++ array.py
@@ -0,0 +1,53 @@
+# problem 1. print your first and last name
+print ('Tess Heiner')
+
+# problem 2. In the array.py create an array
+cars = ['Ford', 'Chrysler', 'Dodge', 'Ram', 'Jeep', 'Chevy', 'GMC']
+
+# problem 3. print array to the console
+print(cars)
+
+# problem 4. print length of array to console
+print(len(cars))
+
+# problem 5. Append Buick to the array
+cars.append('Buick')
+
+# problem 6. print array to the console 
+print(cars)
+
+# problem 7. print 4th element in array
+print(cars[3])
+
+# problem 8. Insert 'Toyota' into element 3
+cars.insert(2, 'Toyota')
+
+# problem 9. print array to console
+print(cars)
+
+# problem 10. Remove element 5 of array
+cars.pop(4)
+
+# problem 11. print array to console
+print(cars)
+
+# problem 12. Sort array in ascending order
+cars.sort()
+
+# problem 13. print array to console
+print(cars)
+
+# problem 14. Sort in descending order
+cars.sort(reverse = True)
+
+# problem 15. print array to console
+print(cars)
+
+# problem 16. create variable 
+my_array_length = len(cars)
+
+# problem 17. create variable 
+array_string = 'The length of my array is'
+
+# problem 18. print array w/ array length
+print(array_string, my_array_length)
